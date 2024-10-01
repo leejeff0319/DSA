d21e06f898543aeec95da8ddebff3057786efbe8
leejeff0319 <leejeff0319@gmail.com>
2024-10-01 19:45:05 -0400

    finish HW1

--- hw1/q1.cpp
+++ hw1/q1.cpp
@@ -6,83 +6,83 @@
 
 int *insert(int *array, int length, int index, int value){
     // no index or invalid input --> return a null pointer
-    if (index < 0 || index > length){
-        return nullptr;
+    if (index < 0 || index > length){                                                       // O(1)
+        return nullptr;                                                                     // O(1)
     }
 
     // an array doesn't already exist
-    if (length == 0){
+    if (length == 0){                                                                       // O(1)
         // declare newArray and allocate memory equal to a single int variable
-        int* newArray = (int*)malloc(sizeof(int));
+        int* newArray = (int*)malloc(sizeof(int));                                          // O(1)
 
         // return null pointer if cannot create newArray (or newArray doesn't exist)
-        if (!newArray){
-            return nullptr;     
+        if (!newArray){                                                                     // O(1)
+            return nullptr;                                                                 // O(1)
         }
         
         // place given value as first index of newArray and return it
-        newArray[0] = value;
-        return newArray;
+        newArray[0] = value;                                                                // O(1)
+        return newArray;                                                                    // O(1)
     } else{
         // if length of array is not 0 --> allocate enough memory for array length + 1 (for insert)
-        int* newArray = (int*)malloc(sizeof(int) * (length+1));
+        int* newArray = (int*)malloc(sizeof(int) * (length+1));                             // O(1)                                       
 
-        if (!newArray){
-            return nullptr;
-        }
+        if (!newArray){                                                                     // O(1)                                                        
+            return nullptr;                                                                 // O(1)                                                           
+        }                                                                   
         
         // Copy original array into newArray up to where new value is inserted
-        for (int i = 0; i < index; ++i){
-            newArray[i] = array[i];
+        for (int i = 0; i < index; ++i){                                                    // O(n)
+            newArray[i] = array[i];                                                         // O(1)
         }
 
         // Insert new value into the specified index
-        newArray[index] = value;
+        newArray[index] = value;                                                            // O(1)
 
         // Copy the rest of the array following the newly inserted value
-        for (int i = index; i<length ; ++i){
-            newArray[i+1] = array[i];
+        for (int i = index; i<length ; ++i){                                                // O(n)
+            newArray[i+1] = array[i];                                                       // O(1)
         }
 
         // Free up memory allocated (prevent memory leak) and return resulting array (with insert)
-        free(array);
+        free(array);                                                                        // O(1)
 
-        return newArray;
+        return newArray;                                                                    // O(1)
     }
 }
 
 int main(){
     // declare constant
-    const int INSERTS_PER_READING = 1000;
+    const int INSERTS_PER_READING = 1000;                                                   // O(1)
     
     // set seed for random
-    srand(time(0));
+    srand(time(0));                                                                         // O(1)
 
     // Open file to save data
-    std::ofstream profilingData("profiling_data.txt");
+    std::ofstream profilingData("profiling_data.txt");                                      // O(1)
     
     // Output header
-    profilingData <<"Array Length           " << "Seconds per insert" << std::endl;
+    profilingData <<"Array Length           " << "Seconds per insert" << std::endl;         // O(1)
 
     // delcare empty array and length variable
-    int* array = nullptr;
-    int length = 0;
+    int* array = nullptr;                                                                   // O(1)
+    int length = 0;                                                                         // O(1)
 
     // Loop through the insert into array 60 times
-    for (int i = 0; i < 60; ++i){
-        auto startTime = std::chrono::system_clock::now();
+    for (int i = 0; i < 60; ++i){                                                           // O(1)
+        auto startTime = std::chrono::system_clock::now();                                  // O(1)
 
         // Insert a random value into a random index (0, length) up to specified amount of times
-        for (int i =0; i < INSERTS_PER_READING; ++i){
-            int index = rand() % (length + 1);
-            int value = rand();
-            array = insert(array, length, index, value);
+        for (int i =0; i < INSERTS_PER_READING; ++i){                                       // O(1)
+            int index = rand() % (length + 1);                                              // O(1)
+            int value = rand();                                                             // O(1)
+            array = insert(array, length, index, value);                                    // O(n)
 
             // increase length of array each time
-            ++length;
+            ++length;                                                                       // O(1)
         }
 
-        auto stopTime = std::chrono::system_clock::now();
+        auto stopTime = std::chrono::system_clock::now();                                   // O(1)
 
         // calculate elapsed time using a double (minimum precision of at least 15 decimals)
         std::chrono::duration<double> elapsedTime = stopTime - startTime;
@@ -90,10 +90,10 @@ int main(){
 
         // write data to file
         profilingData << length << "                  " << 
-        std::fixed << std::setprecision(6) << timePerInsert << std::endl;
+        std::fixed << std::setprecision(6) << timePerInsert << std::endl;                   // O(1)
     }
 
     // free up allocated memory (prevent memory leak)
-    free(array);
+    free(array);                                                                            // O(1)
     return 0;
 }
